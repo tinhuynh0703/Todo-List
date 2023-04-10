431137fdd19e443f478a5c322204f0c791673408
Tin Huynh Tri <118428555+RS-tinht@users.noreply.github.com>
2023-04-10 19:07:21 +0700

    add todo to localStorage

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,9 @@
 import "../styles/styles.css";
 
 export default function App({ Component, pageProps }) {
+  if (typeof window !== "undefined") {
+    // Perform localStorage action
+    const item = localStorage.getItem("key");
+  }
   return <Component {...pageProps} />;
 }
